7138186ae8efd415f531c39156f3b96861f90b36
Halil İbrahim Güneri <gnrbrhm>
2022-01-18 16:47:15 +0300

    Updated.

--- src/background.js
+++ src/background.js
@@ -1,7 +1,9 @@
 'use strict'
 
 import { app, protocol, BrowserWindow } from 'electron'
-import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+// import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
+import createProtocol from 'vue-cli-plugin-electron-builder/lib/createProtocol'
+
 import installExtension, { VUEJS_DEVTOOLS } from 'electron-devtools-installer'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
